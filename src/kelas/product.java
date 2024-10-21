f3c10e206b1fe2b20beef1bed1067c2a2dbb14e6
lutfirahman053 <rahmanrahman8063@gmail.com>
2024-10-21 20:22:21 +0700

    tokokita

---
+++ src/kelas/product.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelas;
+
+/**
+ *
+ * @author hp
+ */
+public class product {
+    int category_id;
+    String category;
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+}
